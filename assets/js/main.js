b71ae9efcbef6d02df7bd39b7392480ce824dfdb
moneypennya <82790001+moneypennya@users.noreply.github.com>
2023-02-27 11:39:16 -0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,18 @@
+window.onload = start;
+var selected;
+function start(){
+    selected = document.getElementById("home");
+    selected.setAttribute('style','background: rgba(33,37,41); color: white !important;');
+    iframe = document.getElementById("mainframe");
+    iframe.addEventListener("load",resize);
+    resize();
+}
+    
+    // Adjusting the iframe height onload event
+function resize(){
+    iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 'px';
+}
+
 function imgToGif(element){
     element.setAttribute('src','assets/images/moneypenny.gif')
 }
@@ -10,19 +25,38 @@ function sidemenu(){
     console.log("clicked");
 }
 
-function goHome(){
-    document.getElementById("mainframe").src = "frames/home.html";
-    console.log("ran");
+function goHome(element){
+    document.getElementById("mainframe").src = "frames/home.html"; 
+    element.setAttribute('style','background: rgba(33,37,41); color: white !important;');
+    if(selected!=element){
+        selected.setAttribute('style','');
+        selected=element;
+    }
 }
 
-function goContact(){
+function goContact(element){
     document.getElementById("mainframe").src = "frames/contact.html";
+    element.setAttribute('style','background: rgba(33,37,41); color: white !important;');
+    if(selected!=element){
+        selected.setAttribute('style','');
+        selected=element;
+    }
 }
 
-function goRental(){
+function goRental(element){
     document.getElementById("mainframe").src = "frames/rental.html";
+    element.setAttribute('style','background: rgba(33,37,41); color: white !important;');
+    if(selected!=element){
+        selected.setAttribute('style','');
+        selected=element;
+    }
 }
 
-function goAbout(){
+function goAbout(element){
     document.getElementById("mainframe").src = "frames/about.html";
+    element.setAttribute('style','background: rgba(33,37,41); color: white !important;');
+    if(selected!=element){
+        selected.setAttribute('style','');
+        selected=element;
+    }
 }
